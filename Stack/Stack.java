7eea2433ffc9a4dd55eb170da6a988a1a4c00684
kushtarbek <dr.kushtarbek@gmail.com>
2023-05-30 12:23:52 -0500

    file changes

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,4 +1,16 @@
-public class Stack {
+public class Stack <V> {
+    private int maxSize;
+    private int top;
+    private V arr[];
 
+    public Stack( int maxSize){
+        this.maxSize = maxSize;
+        this.top = -1; //initially when stack is empty
+        arr = (V[]) new Object[maxSize];//type casting Object[] to V[]
+    }
+
+    public int getCapacity() {
+        return maxSize;
+    }
 
 }
